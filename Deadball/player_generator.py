7e662b2c958b68ac09cd580028914dfa6ab9f560
scotchtape22 <scottwambold@gmail.com>
2020-05-20 10:09:15 -0400

    Update player_generator.py

--- Deadball/player_generator.py
+++ Deadball/player_generator.py
@@ -27,88 +27,88 @@ for t in t_name:
 
 p = 0
 
-while p <= fielders:
-	pf_name = random.choice(f_name)
-	pl_name = random.choice(l_name)
-	town = random.choice(t_name)
-	age = 20 + random.int(1,6)
+while p <= int(fielders):
+	pf_name = random.choice(f_name).rstrip()
+	pl_name = random.choice(l_name).rstrip()
+	town = random.choice(t_name).rstrip()
+	age = 17 + random.randint(1,6)
 
-	bt = random.int(0,9)+random.int(0,9)+15
-	wt = random.int(1,6)+random.int(1,6)+bt
+	bt = random.randint(0,9)+random.randint(0,9)+15
+	wt = random.randint(1,6)+random.randint(1,6)+bt
 	era = "NA"
 
-	hand_roll = random.int(0,9)
+	hand_roll = random.randint(0,9)
 	
-	position_pref = random.choice(["Infield","Outfield"]
+	position_pref = random.choice(["Infield","Outfield"])
 
 	if hand_roll == 0:
 		hand = "switch"
-	else if hand_roll > 0 and hand_roll < 7
+	elif hand_roll > 0 and hand_roll < 7:
 		hand = "right"
 	else:
-		hand = "left "
+		hand = "left"
 
-	perk_roll = random.int(1,6) + random.int(1,6)
+	perk_roll = random.randint(1,6) + random.randint(1,6)
 
 	if perk_roll == 2:
 		perk = "S+,D+"
-	else if perk_roll == 3:
+	elif perk_roll == 3:
 		perk = "S+"
-	else if perk_roll == 4:
+	elif perk_roll == 4:
 		perk = "S+"
-	else if perk_roll < 4 and perk_roll > 10:
+	elif perk_roll < 4 and perk_roll > 10:
 		perk = ""
-	else if perk_roll == 10:
+	elif perk_roll == 10:
 		perk = "P+"
-	else if perk_roll == 11:
+	elif perk_roll == 11:
 		perk = "C+"
 	else:
 		perk = "P++"
 
 
-	print(pf_name+";"+pl_name+";"+town+";"+hand+";"+position_pref+";"+bt+";"+wt+";"+era+";"+perk)
+	print(pf_name+" "+pl_name+";"+town+";"+hand+";"+position_pref+";"+str(bt)+";"+str(wt)+";"+era+";"+perk)
 	p = p + 1
 
 p = 0
 
-while p <= pitcher:
-	pf_name = random.choice(f_name)
-	pl_name = random.choice(l_name)
-	town = random.choice(t_name)
-	age = 20 + random.int(1,6)
+while p <= int(pitcher):
+	pf_name = random.choice(f_name).rstrip()
+	pl_name = random.choice(l_name).rstrip()
+	town = random.choice(t_name).rstrip()
+	age = 17 + random.randint(1,6)
 
-	bt = random.int(0,9)+5
-	wt = random.int(1,6)+bt
+	bt = random.randint(0,9)+5
+	wt = random.randint(1,6)+bt
 
-	position_pref = random.choice(["Starter","Reliever"]
+	position_pref = random.choice(["Starter","Reliever"])
 				      
-	era_roll = random.int(1,8)
+	era_roll = random.randint(1,8)
 	if era_roll == 1:
 		era = "d12"
-	else if era_roll == 2 or era_roll == 3:
+	elif era_roll == 2 or era_roll == 3:
 		era = "d8"
-	else if era_roll == 4 or era_roll == 5 or era_roll == 6:
+	elif era_roll == 4 or era_roll == 5 or era_roll == 6:
 		era = "d4"
 	else:
 		era = "-d4"
 
-	hand_roll = random.int(0,9)
+	hand_roll = random.randint(0,9)
 	if hand_roll == 0:
 		hand = "left"
-	else if hand_roll > 0 and hand_roll < 7
+	elif hand_roll > 0 and hand_roll < 7:
 		hand = "right"
 	else:
-		hand = "left "
+		hand = "left"
 
-	perk_roll = random.int(1,6) + random.int(1,6)
+	perk_roll = random.randint(1,6) + random.randint(1,6)
 
 	if perk_roll == 2:
 		perk = "GB+"
-	else if perk_roll == 3:
+	elif perk_roll == 3:
 		perk = "K+"
-	else if perk_roll < 3 and perk_roll > 11:
+	elif perk_roll < 3 and perk_roll > 11:
 		perk = ""
-	else if perk_roll == 11:
+	elif perk_roll == 11:
 		perk = "ST+"
 	else:
 		perk = "CN+"
@@ -116,5 +116,5 @@ while p <= pitcher:
 	perk = perk+",P-"
 
 
-	print(pf_name+";"+pl_name+";"+town+";"+hand+";"+position_pref+";"+bt+";"+wt+";"+era+";"+perk)
+	print(pf_name+" "+pl_name+";"+town+";"+hand+";"+position_pref+";"+str(bt)+";"+str(wt)+";"+str(era)+";"+perk)
 	p = p + 1
